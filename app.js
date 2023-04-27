b4eeac62064db2f2f7be85e72ee82d45cd4b3b93
PIERO FREUNDT <freundtpiero@gmail.com>
2023-04-26 20:26:36 -0500

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+
+console.log("METODO 01")
+
+const año = new Date().getFullYear();
+let añoNac = prompt("¿En qué año naciste?");
+const edad = añoActual - añoNacimiento;
+console.log(`Tu edad es ${edad} años.`);
+
+console.log("METODO 02")
+
+const año1 = 2023;
+let añoNac1 = prompt("¿En qué año naciste?");
+const edad1 = añoActual1 - añoNacimiento1;
+console.log(`Tu edad es ${edad1} años.`);
+
+
